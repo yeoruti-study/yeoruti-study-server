e2fa1db626ced0d2511619914cd7e42d32605a6c
fkakd1205 <fkakd0102@naver.com>
2022-12-22 22:41:37 +0900

    Create Project

---
+++ src/main/java/com/planner/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package com.planner.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+		System.out.println("HELLO");
+	}
+
+}
