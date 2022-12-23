85f1914b711aac8f0388294be043a4818ba71b71
matt <matt1235@naver.com>
2022-12-23 23:11:57 +0900

    feat : 유저 엔티티 생성 User > 유저 엔티티 클래스 생성

---
+++ src/main/java/com/planner/server/domain/entity/User.java
@@ -0,0 +1,36 @@
+package com.planner.server.domain.entity;
+
+import com.planner.server.Role;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    @NotNull
+    private Long id;
+    private String userName;
+    private String passWord;
+    private String nickName;
+
+    private String profileName;
+    private short profileAge;
+
+    private Role roles;
+    private boolean friendRecommendPermission;
+    private boolean alarmPermission;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Record> recordList = new ArrayList<>();
+}
