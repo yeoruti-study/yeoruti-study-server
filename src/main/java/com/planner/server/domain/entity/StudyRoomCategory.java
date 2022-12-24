14d92bd57ecd059d07628015b3fd017c1b0498fb
matt <matt1235@naver.com>
2022-12-25 04:04:02 +0900

    FEAT : 스터디룸 카테고리 엔티티 생성
    StudyRoomCategory > 모든 스터디룸은 스터디룸 카테고리에 속한다.

---
+++ src/main/java/com/planner/server/domain/entity/StudyRoomCategory.java
@@ -0,0 +1,29 @@
+package com.planner.server.domain.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+public class StudyRoomCategory {
+    @Id @GeneratedValue
+    @Column(name = "study_room_category_id")
+    private Long cid;
+
+    private UUID id;
+
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "studyRoomCategory", cascade = CascadeType.ALL)
+    private List<StudyRoom> studyRooms = new ArrayList<>();
+
+    public void addStudyRoom(StudyRoom studyRoom){
+        this.studyRooms.add(studyRoom);
+        studyRoom.setStudyRoomCategory(this);
+    }
+}
