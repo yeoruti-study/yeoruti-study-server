b14673514a97f89cf7966627eb9418e7c72e7e24
fkakd1205 <fkakd0102@naver.com>
2022-12-25 15:03:32 +0900

    feat : friend 엔티티 생성 FriendEntity => 스터디 친구 엔티티 생성

---
+++ src/main/java/com/planner/server/domain/friend/FriendEntity.java
@@ -0,0 +1,45 @@
+package com.planner.server.domain.friend;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.planner.server.domain.user.entity.UserEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Table
+@Entity(name = "friend")
+@Getter
+public class FriendEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cid;
+
+    @Column(name = "friend_id")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity friend;
+
+    private Boolean friendAcceptance;
+    
+    private LocalDateTime createdAt;
+}
