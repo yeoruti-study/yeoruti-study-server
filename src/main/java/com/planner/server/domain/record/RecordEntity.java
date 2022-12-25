150222dd753c05bd280d0344f590a91dc63f4bca
fkakd1205 <fkakd0102@naver.com>
2022-12-25 15:03:05 +0900

    feat : record 엔티티 생성 RecordEntity => 스터디 기록 엔티티 생성

---
+++ src/main/java/com/planner/server/domain/record/RecordEntity.java
@@ -0,0 +1,44 @@
+package com.planner.server.domain.record;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.planner.server.domain.user.entity.UserEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "record")
+@Getter
+public class RecordEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cid;
+    
+    @Column(name = "record_id")
+    private UUID id;
+    
+    private LocalDateTime startTime;
+    
+    private LocalDateTime endTime;
+
+    private Duration totalStudyTime;
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+}
