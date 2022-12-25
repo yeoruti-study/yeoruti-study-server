def8900814331aa094b6450b77d78d3a5950ce0c
fkakd1205 <fkakd0102@naver.com>
2022-12-25 15:06:11 +0900

    feat : study goal 엔티티 생성 StudyGoalEntity => 스터디 목표 엔티티 생성

---
+++ src/main/java/com/planner/server/domain/study_goal/StudyGoalEntity.java
@@ -0,0 +1,48 @@
+package com.planner.server.domain.study_goal;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.planner.server.domain.user.entity.UserEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "study_goal")
+@Getter
+public class StudyGoalEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cid;
+
+    @Column(name = "study_goal_id")
+    private UUID id;
+
+    private String goalTitle;
+
+    private String goalDetail;
+
+    private Duration goalTime;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+}
