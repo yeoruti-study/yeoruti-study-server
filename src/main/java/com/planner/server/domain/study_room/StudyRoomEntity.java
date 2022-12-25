f6e1ca2852f96fa5fd4a72dbc9ff89616598a784
fkakd1205 <fkakd0102@naver.com>
2022-12-25 15:06:33 +0900

    feat : study room 엔티티 생성 StudyRoomEntity => 스터디룸 엔티티생성

---
+++ src/main/java/com/planner/server/domain/study_room/StudyRoomEntity.java
@@ -0,0 +1,77 @@
+package com.planner.server.domain.study_room;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.planner.server.domain.room_chat.RoomChatEntity;
+import com.planner.server.domain.room_user.RoomUserEntity;
+import com.planner.server.domain.study_category.StudyCategoryEntity;
+import com.planner.server.domain.user.entity.UserEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "study_room")
+@Getter
+public class StudyRoomEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cid;
+
+    @Column(name = "study_room_id")
+    private UUID id;
+
+    private String name;
+
+    private Integer maximumNumberOfPeople;
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_category_id")
+    private StudyCategoryEntity studyCategory;
+
+    private Duration studyGoalTime;
+
+    private String roomPassword;
+
+    private LocalDateTime createdAt;
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "studyRoom")
+    List<RoomUserEntity> roomUsers = new ArrayList<RoomUserEntity>();
+
+    @OneToMany(mappedBy = "studyRoom")
+    List<RoomChatEntity> roomChats = new ArrayList<RoomChatEntity>();
+
+    public void addRoomUser(RoomUserEntity roomUser) {
+        this.roomUsers.add(roomUser);
+        roomUser.setStudyRoom(this);
+    }
+
+    public void addRoomChat(RoomChatEntity roomChat) {
+        this.roomChats.add(roomChat);
+        roomChat.setStudyRoom(this);
+    }
+}
