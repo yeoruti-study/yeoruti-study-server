4e678f0481d247a2c9dd53361040611583d8ff82
fkakd1205 <fkakd0102@naver.com>
2023-02-09 22:02:40 +0900

    fix : test user dto 제거

--- src/main/java/com/planner/server/domain/room_user/dto/RoomUserReqDto.java
+++ src/main/java/com/planner/server/domain/room_user/dto/RoomUserReqDto.java
@@ -4,8 +4,8 @@ import java.util.UUID;
 
 import com.planner.server.domain.room_user.entity.RoomUser;
 import com.planner.server.domain.study_room.dto.StudyRoomResDto;
+import com.planner.server.domain.user.dto.UserResDto;
 
-import com.planner.server.domain.user.dto.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,11 +19,11 @@ import lombok.ToString;
 @NoArgsConstructor
 public class RoomUserReqDto {
     private UUID id;
-    private UserDto userDto;
+    private UserResDto userDto;
     private StudyRoomResDto studyRoomDto;
 
     public static RoomUserReqDto toDto(RoomUser entity) {
-        UserDto userDto = UserDto.toDto(entity.getUser());
+        UserResDto userDto = UserResDto.toDto(entity.getUser());
         StudyRoomResDto studyRoomDto = StudyRoomResDto.toDto(entity.getStudyRoom());
 
         return RoomUserReqDto.builder()
