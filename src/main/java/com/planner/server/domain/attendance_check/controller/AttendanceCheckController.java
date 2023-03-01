c4e018fecd791180f242ce8bfde730fd2a9febb2
fkakd1205 <fkakd0102@naver.com>
2023-03-01 22:53:57 +0900

    refactor : [attendance_check] security context holder에서 조회한 user정보를 사용해 api 수정. 출첵 단일제거 api 제거 AttendanceCheckRepository : findByDateRange => userId를 함께 전달하도록 수정 AttendanceCheckService : createOne, searchListByUserId => security context holder에서 조회한 user정보를 사용 AttendanceCheckService : deleteOne => 출석체크 단일제거 api 제거

--- src/main/java/com/planner/server/domain/attendance_check/controller/AttendanceCheckController.java
+++ src/main/java/com/planner/server/domain/attendance_check/controller/AttendanceCheckController.java
@@ -1,14 +1,11 @@
 package com.planner.server.domain.attendance_check.controller;
 
-import com.planner.server.domain.attendance_check.dto.AttendanceCheckReqDto;
 import com.planner.server.domain.attendance_check.service.AttendanceCheckService;
 import com.planner.server.domain.message.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/attendance-check")
@@ -17,11 +14,11 @@ public class AttendanceCheckController {
     private final AttendanceCheckService attendanceCheckService;
 
     @PostMapping("/one")
-    public ResponseEntity<?> createOne(@RequestBody AttendanceCheckReqDto attendanceCheckDto){
+    public ResponseEntity<?> createOne(){
         Message message = new Message();
 
         try {
-            attendanceCheckService.createOne(attendanceCheckDto);
+            attendanceCheckService.createOne();
             message.setStatus(HttpStatus.OK);
             message.setMessage("success");
         } catch (Exception e) {
@@ -32,28 +29,12 @@ public class AttendanceCheckController {
         return new ResponseEntity<>(message, message.getStatus());
     }
 
-    @GetMapping("/list/user/{userId}")
-    public ResponseEntity<?> searchListByUserId(@PathVariable(name = "userId") UUID userId){
+    @GetMapping("/list/user")
+    public ResponseEntity<?> searchListByUserId(){
         Message message = new Message();
 
         try {
-            message.setData(attendanceCheckService.searchListByUserId(userId));
-            message.setStatus(HttpStatus.OK);
-            message.setMessage("success");
-        } catch (Exception e) {
-            message.setStatus(HttpStatus.BAD_REQUEST);
-            message.setMessage("error");
-            message.setMemo(e.getMessage());
-        }
-        return new ResponseEntity<>(message, message.getStatus());
-    }
-
-    @DeleteMapping("/one/{attendanceCheckId}")
-    public ResponseEntity<?> deleteOne(@PathVariable(name = "attendanceCheckId") UUID userId){
-        Message message = new Message();
-
-        try {
-            attendanceCheckService.deleteOne(userId);
+            message.setData(attendanceCheckService.searchListByUserId());
             message.setStatus(HttpStatus.OK);
             message.setMessage("success");
         } catch (Exception e) {
